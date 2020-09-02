d245874ff6e69cad1c7c251ef3b2c6115a91ec77
Sara <scearc930@gmail.com>
2020-09-01 20:52:39 -0400

    added follower cards

--- github-usercard-react/src/Followers.js
+++ github-usercard-react/src/Followers.js
@@ -1,44 +1,49 @@
 import React from 'react'
 import axios from 'axios'
-import {Card, Avatar, CardHeader} from '@material-ui/core'
+import { Card, Avatar, CardHeader } from '@material-ui/core'
 
 class Followers extends React.Component {
     constructor() {
-      super();
-      this.state = {
-        followers:[]
-      }
+        super();
+        this.state = {
+            followers: []
+        }
     }
-  
+
     componentDidMount() {
-      axios.get('https://api.github.com/users/cearc-sara/followers')
-      .then((res) => {
-        console.log(res.data)
-       this.setState({
-           followers: res.data
-       })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+        axios.get('https://api.github.com/users/cearc-sara/followers')
+            .then((res) => {
+                console.log(res.data)
+                this.setState({
+                    followers: res.data
+                })
+            })
+
+
+            .catch((err) => {
+                console.log(err)
+            })
     }
     render() {
-    return (
-      <Card variant='outlined' className='card'>
-        <CardHeader avatar={<Avatar>S</Avatar>} title={this.state.name}/>
-        <img src={this.state.followers.image} alt='profile pic' className='image'/>
-        <div className='card-info'>
-          <h3 className='name'>{this.state.followers.name}</h3>
-          <p className='username'>{this.state.followers.username}</p>
-          
-          <p>Profile: 
-            <a href={this.state.followers.profile} className='profile-link'> {this.state.followers.profile}</a></p>
-          
-        </div>
-    </Card>
-    )
+        return (
+            <>
+            {
+                this.state.followers.map((follower) => {
+                    return(
+                        <Card variant='outlined' className='follower-card'>
+                            <h3>{follower.name}</h3>
+                            <img src={follower.avatar_url} alt='follower pic' className='image'/>
+                            <div>
+                            <p className='username'>{follower.login}</p>
+                            <p>Profile: 
+                                <a href={follower.url} className='profile-link'> {follower.url}</a></p>
+                            </div>
+                        </Card>
+                    )
+                })
+            }
+            </>
+        )
     }
-  }
-  
+}
   export default Followers;
-  
